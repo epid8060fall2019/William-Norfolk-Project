e648e25fa13bbae609afd9fcd63efb6fbda8404e
William Norfolk <wnorfolk4477@gmail.com>
2019-09-09 23:55:33 -0400

    Processing_1

---
+++ code/processing_code/WQprocessingscript.R
@@ -0,0 +1,21 @@
+####################
+#
+#this script loads the raw data, processes and cleans it 
+#and saves it as Rds file in the processed_data folder
+
+#load needed packages. make sure they are installed.
+
+library(readxl)
+library(dplyr)
+library(tidyverse)
+
+
+#load data. path is relative to project directory.
+wqrawdata <- readxl::read_excel("./data/raw_data/RAW_WQ DATA.xlsx")
+
+#take a look at the data
+dplyr::glimpse(wqrawdata)
+
+#TO DO-make NA = na in r try replace_with_na() function, 
+#   remove timestamp variabe, remove ...13 variable, add ocean or 
+#   bay variable, add age-group variable to group cohorts.
