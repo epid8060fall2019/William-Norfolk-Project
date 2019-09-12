0d341b56da74ee4419e0767f93bc140378b5d502
William Norfolk <wnorfolk4477@gmail.com>
2019-09-11 23:11:37 -0400

    Introduction_2

--- code/processing_code/WQprocessingscript.R
+++ code/processing_code/WQprocessingscript.R
@@ -1,5 +1,5 @@
 ####################
-#
+
 #this script loads the raw data, processes and cleans it 
 #and saves it as Rds file in the processed_data folder
 
@@ -16,6 +16,18 @@ wqrawdata <- readxl::read_excel("./data/raw_data/RAW_WQ DATA.xlsx")
 #take a look at the data
 dplyr::glimpse(wqrawdata)
 
+#We should have 11 variables in our data set but it seems that 13 have 
+#been loaded. The variables "Timestamp" and "...13" are errors and should 
+#be dropped.
+
+wqrawdata_11 <- select(wqrawdata, -c(Timestamp, ...13))
+
+
+
+
+
 #TO DO-make NA = na in r try replace_with_na() function, 
 #   remove timestamp variabe, remove ...13 variable, add ocean or 
 #   bay variable, add age-group variable to group cohorts.
+
+#
